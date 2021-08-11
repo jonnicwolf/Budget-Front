9d911fc5aa0dd7a689fa4bdc038c2daac57e477d
Jon Narine <jonnicwolf@gmail.com>
2021-08-11 17:19:31 -0400

    handle value change with state

--- src/components/NewBudgetForm.js
+++ src/components/NewBudgetForm.js
@@ -1,49 +1,50 @@
 import React, { useState, useEffect } from 'react';
 
 const NewBudgetForm = () => {
-    const [form, setForm] = useState({
-        date: '',
-        name: '',
-        amount: 0,
-        from: ''
-    })
+    const [inputString, setInputString] = useState('')
+    const [inputNumber, setInputNumber] = useState(0);
 
-    // handleChange_String = (e) => {
-    // e.preventDefault()
+    const handleChange_String = (e) => {
+        setInputString(e.target.value)
+    }
     
-    //  }
+    const handleChange_Number = (e) => {
+        const number = e.target.value
+        setInputNumber(number)
+    }
+    
     return (
         <div>
             <form >
                 <label htmlFor="date">Date of transaction: </label>
                 <input
                     placeholder='mm/dd/yyyy'
+                    onChange={handleChange_String}
                     type="text"
-                    value={`foo`}
                     name='date'
                     id='date'
                 />
                 <label htmlFor="name">Item/Service: </label>
                 <input
                     placeholder='eg. Gas'
+                    onChange={ handleChange_String}
                     type="text"
-                    value={`foo`}
                     name='name'
                     id='name'
                 />
                 <label htmlFor="amount">Amount: </label>
                 <input
                     placeholder='$'
+                    onChange={ handleChange_Number}
                     type="text"
-                    value={`foo$`}
                     name='amount'
                     id='amount'
                 />
                 <label htmlFor="from">From: </label>
                 <input
                     placeholder='Expense/Income'
+                    onChange={handleChange_String}
                     type="text"
-                    value={'foo'}
                     name='from'
                     id='from'
                 />
